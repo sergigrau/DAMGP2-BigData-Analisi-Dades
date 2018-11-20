bed460bcf9be7d1dad4bbcfb27cd70d02ac1f859
sergi grau <sergi.grau@fje.edu>
2018-11-20 12:24:18 +0100

    noves pelis

--- 02_recomanacions/M01_preferencies.py
+++ 02_recomanacions/M01_preferencies.py
@@ -2,10 +2,10 @@
 # author sergi.grau@fje.edu
 # version 1.0 20.2.2017
 
-critiques = {'Laura': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5, 'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5, 'The Night Listener': 3.0},
-             'Pere': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5, 'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0, 'You, Me and Dupree': 3.5},
-             'Sergi': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0, 'Superman Returns': 3.5, 'The Night Listener': 4.0},
-             'Claudia': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0, 'The Night Listener': 4.5, 'Superman Returns': 4.0, 'You, Me and Dupree': 2.5},
-             'Miquel': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Listener': 3.0, 'You, Me and Dupree': 2.0},
-             'Jordi': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
-             'Carles': {'Snakes on a Plane': 4.5, 'You, Me and Dupree': 1.0, 'Superman Returns': 4.0}}
+critiques = {'Laura': {'Jurasic World': 2.5, 'Alien': 3.5, 'Terminator': 3.0, 'Superman Returns': 3.5, 'Spiderman': 2.5, 'Harry Potter': 3.0},
+             'Pere': {'Jurasic World': 3.0, 'Alien': 3.5, 'Terminator': 1.5, 'Superman Returns': 5.0, 'Harry Potter': 3.0, 'Spiderman': 3.5},
+             'Sergi': {'Jurasic World': 2.5, 'Alien': 3.0, 'Superman Returns': 3.5, 'Harry Potter': 4.0},
+             'Claudia': {'Alien': 3.5, 'Terminator': 3.0, 'Harry Potter': 4.5, 'Superman Returns': 4.0, 'Spiderman': 2.5},
+             'Miquel': {'Jurasic World': 3.0, 'Alien': 4.0, 'Terminator': 2.0, 'Superman Returns': 3.0, 'Harry Potter': 3.0, 'Spiderman': 2.0},
+             'Jordi': {'Jurasic World': 3.0, 'Alien': 4.0, 'Harry Potter': 3.0, 'Superman Returns': 5.0, 'Spiderman': 3.5},
+             'Carles': {'Alien': 4.5, 'Spiderman': 1.0, 'Superman Returns': 4.0}}
